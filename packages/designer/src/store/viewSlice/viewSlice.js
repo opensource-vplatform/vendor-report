572fecd10d95777425d981f7e11faa8df199a1c8
onlylife59 <octonber@qq.com>
2023-12-10 19:21:06 +0800

    代码提交

--- packages/designer/src/store/viewSlice/viewSlice.js
+++ packages/designer/src/store/viewSlice/viewSlice.js
@@ -7,50 +7,63 @@ export const fontSlice = createSlice({
     name: 'viewSlice',
     initialState: {
         spread: null,
-        showColumnHeader: true,
-        showRowHeader: true,
-        showHideHGridLine: true,
-        showHideVGridLine: true,
-        showHide
+        //显示列标题
+        colHeaderVisible: true,
+        //显示行标题
+        rowHeaderVisible: true,
+        //显示水平网格线
+        showHorizontalGridline: true,
+        //显示垂直网格线
+        showVerticalGridline: true,
+        //显示新增sheet图标
+        newTabVisible: true,
     },
     reducers: {
         toggleFontStyle(state) {
-            state.fontStyle = state.fontStyle === 'normal' ? 'italic' : 'normal'
+            state.fontStyle =
+                state.fontStyle === 'normal' ? 'italic' : 'normal';
         },
         toggleFontWeight(state) {
-            state.fontWeight = state.fontWeight === 'normal' ? 'bold' : 'normal'
+            state.fontWeight =
+                state.fontWeight === 'normal' ? 'bold' : 'normal';
         },
-        toggleFont(state,action) {
+        toggleFont(state, action) {
             //切换状态为布尔值的属性
-            const hasOwnProperty = Object.prototype.hasOwnProperty
+            const hasOwnProperty = Object.prototype.hasOwnProperty;
             if (Array.isArray(action.payload)) {
-                action.payload.forEach(function(key) {
-                    if (hasOwnProperty.call(state,key)) {
-                        state[key] = !state[key]
+                action.payload.forEach(function (key) {
+                    if (hasOwnProperty.call(state, key)) {
+                        state[key] = !state[key];
                     }
-                })
+                });
             } else {
-                if (hasOwnProperty.call(state,action.payload)) {
-                    state[action.payload] = !state[action.payload]
+                if (hasOwnProperty.call(state, action.payload)) {
+                    state[action.payload] = !state[action.payload];
                 }
             }
         },
         updateFont(state, action) {
-            const font = action.payload.font
-            Object.entries(font).forEach(function([key, value]) {
-                state[key] = value
-            })
+            const font = action.payload.font;
+            Object.entries(font).forEach(function ([key, value]) {
+                state[key] = value;
+            });
         },
         resetCellFont(state) {
             //切换单元格会重置状态
-            Object.assign(state, parseFont(state.spread))
-        }
+            Object.assign(state, parseFont(state.spread));
+        },
     },
     extraReducers: (builder) => {
         builder.addCase(setSpread, (state, action) => {
-            state.spread = action.payload.spread
+            state.spread = action.payload.spread;
         });
     },
-})
-export const { toggleFontWeight, resetCellFont, toggleFontStyle, updateFont,toggleFont } = fontSlice.actions
-export default fontSlice.reducer
+});
+export const {
+    toggleFontWeight,
+    resetCellFont,
+    toggleFontStyle,
+    updateFont,
+    toggleFont,
+} = fontSlice.actions;
+export default fontSlice.reducer;
