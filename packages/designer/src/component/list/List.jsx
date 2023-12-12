96c20f3807546078c0dc842e94f6ef4ba238a799
fanghm <675712413@qq.com>
2023-12-12 18:06:22 +0800

    封装List组件

--- packages/designer/src/component/list/List.jsx
+++ packages/designer/src/component/list/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import './list.scss';
 const List = ({
     values, // 数据源
@@ -6,30 +7,41 @@ const List = ({
     height = 'auto',
     selectedValue,
     onChange,
+    isHasInput = false,
 }) => {
+    const [filterText, setFilterText] = useState(selectedValue);
+
     const handleItemClick = (value) => {
         onChange(value);
-        console.log('value :>> ', value);
+        setFilterText(value);
     };
 
     return (
-        <div className='list' style={{ width, height }}>
-            {values.map((value) => {
-                const isSelected = selectedValue === value;
-                const itemClassName = isSelected
-                    ? 'listItemSelected'
-                    : 'listItem';
-
-                return (
-                    <div
-                        key={value}
-                        className={itemClassName}
-                        onClick={() => handleItemClick(value)}
-                    >
-                        {value}
-                    </div>
-                );
-            })}
+        <div style={{ width, height, margin: '5px 0px' }}>
+            {isHasInput && (
+                <input
+                    className='listInput'
+                    onChange={(e) => setFilterText(e.target.value)}
+                    value={filterText}
+                />
+            )}
+            <div className='list'>
+                {values.map((value) => {
+                    const isSelected = selectedValue === value;
+                    const itemClassName = isSelected
+                        ? 'listItemSelected'
+                        : 'listItem';
+                    return (
+                        <div
+                            key={value}
+                            className={itemClassName}
+                            onClick={() => handleItemClick(value)}
+                        >
+                            {value}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
