abfbb2639e51b2bb9cb8c97a0a37bba17262cfc0
zhougege520 <2912354876@qq.com>
2024-01-26 16:43:12 +0800

    分组字段求和功能开发

--- packages/designer/src/component/reportDesignWizard/reportTemplate/right.jsx
+++ packages/designer/src/component/reportDesignWizard/reportTemplate/right.jsx
@@ -20,6 +20,15 @@ import DesignerContext from '../../../DesignerContext';
 const GC = getNamespace();
 const spreadNS = GC.Spread.Sheets;
 
+function indexToColumn(index) {
+    let column = '';
+    while (index >= 0) {
+        column = String.fromCharCode((index % 26) + 65) + column;
+        index = Math.floor(index / 26) - 1;
+    }
+    return column;
+}
+
 function preview(params) {
     const { value, field = [], exclude = [], spread, groups = [] } = params;
 
@@ -46,11 +55,11 @@ function preview(params) {
         sheet.autoMerge(range, 0);
         sheet.tables.remove(table);
     });
-
     spread.resumePaint();
     spread.suspendPaint();
     //表格字段数量大于0才创建表格
     const colCount = tableColumns.length;
+    let _table = null;
     if (colCount > 0) {
         //如果sheet的行数小于当前表格所绑定的数据长度，需要增加sheet的行数
         const datasource = sheet.getDataSource().getSource();
@@ -77,6 +86,7 @@ function preview(params) {
         table.autoGenerateColumns(false);
         table.bindColumns(tableColumns);
         table.bindingPath(value);
+        _table = table;
         for (let i = 0; i < colCount; i++) {
             table.filterButtonVisible(i, false);
         }
@@ -84,11 +94,84 @@ function preview(params) {
             try {
                 const range = table.range();
                 range.colCount = groups.length;
-                sheet.autoMerge(range, 1, 1, 3, 1);
+                //sheet.autoMerge(range, 1, 1, 3, 1);
             } catch (error) {}
         }
     }
+    spread.resumePaint();
 
+    //对表格区域的数据进行合并
+    spread.suspendPaint();
+    if (Array.isArray(window.spansInfo)) {
+        const { row, col } = _table.range();
+        let startRow = row + 1;
+        const groupStartRows = groups.map(() => startRow);
+        let preGroupIndex = -1;
+        window.spansInfo.forEach(function ({
+            datasLen = 0,
+            virtualDatasLen,
+            field,
+            groupCode,
+            isNull,
+        }) {
+            let endCol = field.length + col + groups.length;
+            const groupIndex = groups.findIndex(
+                ({ code }) => code === groupCode
+            );
+            for (let i = 0; i < datasLen; i++) {
+                let startCol = col + groups.length;
+                let spanRowCount = virtualDatasLen + 1;
+                let hRowStartIndex = virtualDatasLen;
+                while (startCol < endCol) {
+                    //纵向合并
+                    sheet.addSpan(startRow + i, startCol, spanRowCount, 1);
+                    //横向合并
+                    if (hRowStartIndex > 0) {
+                        sheet.addSpan(
+                            startRow + hRowStartIndex + i,
+                            startCol + 1,
+                            1,
+                            endCol - startCol - 1
+                        );
+                    }
+                    startCol++;
+                    spanRowCount--;
+                    hRowStartIndex--;
+                }
+                startRow += virtualDatasLen;
+            }
+            startRow += datasLen + 1;
+            if (groupCode) {
+                //对每一组的汇总行进行横向合并
+                const _row = startRow - 1;
+                const _col = col + groupIndex + 1;
+                const rowCount = 1;
+                const colCount = endCol - 1 - groupIndex;
+                sheet.addSpan(_row, _col, rowCount, colCount);
+
+                //对分组名进行合并
+                const groupRowStart = groupStartRows[groupIndex];
+                sheet.addSpan(
+                    groupRowStart,
+                    col + groupIndex,
+                    startRow - groupRowStart,
+                    1
+                );
+                groupStartRows[groupIndex] = startRow;
+                let j = 1;
+                for (let i = 0; i < groupStartRows.length; i++) {
+                    if (i > groupIndex) {
+                        groupStartRows[i] = groupStartRows[i] + j;
+                        j++;
+                    }
+                }
+                preGroupIndex = groupIndex;
+            } else {
+                //对最后的汇总结果行横向合并
+                sheet.addSpan(startRow - 1, col, 1, endCol);
+            }
+        });
+    }
     spread.resumePaint();
 }
 
@@ -143,7 +226,11 @@ export default function Index(props) {
                 const sheet = spread.getActiveSheet();
                 const dataSource = JSON.parse(JSON.stringify(previewViewDatas));
                 if (groups.length > 0) {
-                    const { datas } = sortData(dataSource[value], groups);
+                    const { datas } = sortData(
+                        dataSource[value],
+                        groups,
+                        field
+                    );
                     dataSource[value] = datas;
                 }
                 const source = new spreadNS.Bindings.CellBindingSource(
--- packages/designer/src/utils/commonUtil.js
+++ packages/designer/src/utils/commonUtil.js
@@ -96,13 +96,146 @@ export function getActiveSheetTablesPath(params) {
     }, {});
 }
 
-export function sortData(datas, groups) {
+function sum(datas, groupFields = [], fields = []) {
+    const results = [];
+    const countCol = ['quantity'];
+    const field = fields.filter(function ({ code }) {
+        return !groupFields.includes(code) && !countCol.includes(code);
+    });
+    const spansInfo = [];
+    const fieldLen = field.length;
+    function handleDatas(groups) {
+        let lastChild = null;
+        groups.forEach(function ({
+            childrenGroups,
+            allChildrens,
+            groupCode,
+            groupName,
+        }) {
+            if (childrenGroups) {
+                lastChild = handleDatas(childrenGroups);
+                spansInfo.push({
+                    field,
+                    isNull: true,
+                    groupCode,
+                });
+            } else {
+                let virtualDatasLen = 0;
+                allChildrens.forEach(function (item) {
+                    lastChild = item;
+                    const virtualDatas = [];
+                    const preFiledData = {};
+                    groupFields.forEach(function (field) {
+                        preFiledData[field] = item[field];
+                    });
+                    for (let i = 0; i < fieldLen - 1; i++) {
+                        const currentFieldCode = field[i].code;
+                        preFiledData[currentFieldCode] = item[currentFieldCode];
+                        const nextFieldCode = field[i + 1].code;
+                        const virtualData = {
+                            ...preFiledData,
+                            [nextFieldCode]: '小计',
+                        };
+                        Object.defineProperty(virtualData, 'quantity', {
+                            get() {
+                                return item['quantity'];
+                            },
+                        });
+                        virtualDatas.unshift(virtualData);
+                    }
+                    virtualDatasLen = virtualDatas.length;
+                    results.push(item, ...virtualDatas);
+                });
+                spansInfo.push({
+                    virtualDatasLen,
+                    datasLen: allChildrens.length,
+                    field,
+                    groupCode,
+                });
+            }
+            const virtualData = {};
+            if (groupCode) {
+                const index = groupFields.indexOf(groupCode);
+                groupFields.forEach(function (item, curIndex) {
+                    curIndex <= index && (virtualData[item] = lastChild[item]);
+                });
+                let code = groupFields[index + 1];
+                if (index + 1 === groupFields.length) {
+                    code = field[0].code;
+                }
+
+                virtualData[groupCode] = groupName;
+                virtualData[code] = '小计';
+            } else {
+                virtualData[groupFields[0]] = '小计';
+            }
+            Object.defineProperty(virtualData, 'quantity', {
+                get() {
+                    return allChildrens.reduce(function (res, item) {
+                        return res + item['quantity'];
+                    }, 0);
+                },
+            });
+            results.push(virtualData);
+        });
+        return lastChild;
+    }
+    handleDatas(datas);
+    window.spansInfo = spansInfo;
+    console.log(spansInfo);
+    return results;
+}
+
+function groupData(datas, groups) {
+    function group(datas, groups) {
+        const cobyGroups = [...groups];
+        const groupedDatas = new Map();
+        const groupField = cobyGroups.shift();
+        datas.forEach(function (data) {
+            const groupName = data[groupField];
+            if (!groupedDatas.has(groupName)) {
+                groupedDatas.set(groupName, {
+                    groupName,
+                    groupCode: groupField,
+                    allChildrens: [],
+                });
+            }
+            const currentGroups = groupedDatas.get(groupName);
+            currentGroups.allChildrens.push(data);
+        });
+
+        groupedDatas.forEach(function (groupInfo) {
+            if (cobyGroups.length > 0 && groupInfo.allChildrens.length > 0) {
+                groupInfo.childrenGroups = group(
+                    groupInfo.allChildrens,
+                    cobyGroups
+                );
+            }
+        });
+        return groupedDatas;
+    }
+    const groupedDatas = new Map([
+        [
+            '总',
+            {
+                groupName: '总',
+                groupCode: null,
+                allChildrens: [...datas],
+                childrenGroups: group(datas, groups),
+            },
+        ],
+    ]);
+    return groupedDatas;
+}
+
+export function sortData(datas, groups, fields) {
     //初始化分组数据(二维数组)
     const groupedDatas = [datas];
     const groupFields = groups.map(({ code }) => code);
     const spans = []; //二维数组，用于收集每一组合并单元格的数量，[[]]
     let groupNumber = 0;
-
+    const resultDatas = groupData(datas, groupFields);
+    const sumRes = sum(resultDatas, groupFields, fields);
     while (groupFields.length > 0) {
         spans[groupNumber] = [];
         const field = groupFields.shift();
@@ -135,8 +268,9 @@ export function sortData(datas, groups) {
 
     //合并分组后的数据
     const result = groupedDatas.reduce((res, cur) => [...res, ...cur], []);
+    window.sumRes = sumRes;
     return {
-        datas: result,
+        datas: sumRes || result,
         spans,
     };
 }
