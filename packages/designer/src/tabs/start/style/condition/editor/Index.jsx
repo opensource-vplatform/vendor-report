d3cd10dbc6300eea8f7e612320549a3bf50cb6f3
Matoah <octonber@qq.com>
2024-04-03 17:46:13 +0800

    条件格式

--- packages/designer/src/tabs/start/style/condition/editor/Index.jsx
+++ packages/designer/src/tabs/start/style/condition/editor/Index.jsx
@@ -8,7 +8,13 @@ import {
   Wrap,
 } from '../Components';
 import { getFormatTypes } from '../metadata';
+import FormatAbove from './formatAbove/Index';
+import FormatContain from './formatContain/Index';
 import FormatOnValue from './formatOnValue/Index';
+import FormatRankedValue from './formatRankedValue/Index';
+import FormatUnique from './formatUnique/Index';
+import UseFormula from './useFormula/Index';
+import UseRowColumnStates from './useRowColumnStates/Index';
 
 export default function (props) {
     const { type, onConfirm, onCancel } = props;
@@ -28,8 +34,31 @@ export default function (props) {
                     selectedValue={data.type}
                     datas={formatTypes}
                     style={{ backgroundColor: 'white' }}
+                    onChange={(val) =>
+                        setData((data) => {
+                            return { ...data, type: val };
+                        })
+                    }
                 ></List>
-                {data.type=='formatOnValue' ? <FormatOnValue></FormatOnValue>:null}
+                {data.type == 'formatAbove' ? (
+                    <FormatAbove></FormatAbove>
+                ) : null}
+                {data.type == 'formatContain' ? (
+                    <FormatContain></FormatContain>
+                ) : null}
+                {data.type == 'formatOnValue' ? (
+                    <FormatOnValue></FormatOnValue>
+                ) : null}
+                {data.type == 'formatRankedValue' ? (
+                    <FormatRankedValue></FormatRankedValue>
+                ) : null}
+                {data.type == 'formatUnique' ? (
+                    <FormatUnique></FormatUnique>
+                ) : null}
+                {data.type == 'useFormula' ? <UseFormula></UseFormula> : null}
+                {data.type == 'useRowColumnStates' ? (
+                    <UseRowColumnStates></UseRowColumnStates>
+                ) : null}
             </Wrap>
         </OperationDialog>
     );
