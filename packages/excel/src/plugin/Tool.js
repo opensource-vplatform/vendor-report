483a0031235e87b9681fbfabcba49ba4830a2b5a
Matoah <octonber@qq.com>
2024-05-10 14:46:16 +0800

    代码提交

--- packages/excel/src/plugin/Tool.js
+++ packages/excel/src/plugin/Tool.js
@@ -15,12 +15,40 @@ export default class Tool {
         this.totalPagesHandler = handler;
     }
 
+    setUnionDatasourceHandler(handler){
+        this.unionDatasourceHandler = handler;
+    }
+
+    setDataIndex(handler){
+        this.dataIndexHandler = handler;
+    }
+
+    /**
+     * 获取字段下标（在工作表中的起始位置）
+     * @param {*} tableCode 
+     * @param {*} fieldCode 
+     * @returns {row,col}
+     */
     getFieldIndex(tableCode, fieldCode) {
         return this.fieldIndexHandler(tableCode, fieldCode);
     }
 
+    /**
+     * 获取字段下标（在联合数据源中的起始位置）
+     * @param {*} tableCode 
+     * @param {*} fieldCode 
+     * @return Integer
+     */
+    getDataIndex(tableCode, fieldCode){
+        return this.dataIndexHandler(tableCode, fieldCode);
+    }
+
     getDataCount(tableCode) {
         return this.dataCountHandler(tableCode);
+    }
+
+    getUnionDatasource(){
+        return this.unionDatasourceHandler();
     }
 
     getPage() {
