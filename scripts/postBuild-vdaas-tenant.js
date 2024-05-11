ef837c2165a3adfe2ca9d7befaf7235a95a809e1
Matoah <octonber@qq.com>
2024-05-11 17:41:16 +0800

    代码提交

--- scripts/postBuild-vdaas-tenant.js
+++ scripts/postBuild-vdaas-tenant.js
@@ -31,6 +31,8 @@ const getDirNames = function (sourcePath) {
 
 let dirnames = getDirNames(designerDist);
 
+dirnames.push("image.png");
+
 dirnames.forEach((dirname) => {
     buildUtils.copy(
         path.resolve(designerDist, dirname),
