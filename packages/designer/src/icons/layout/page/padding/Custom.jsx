4432fa39a4bc925d6dc8c0c6ccb7545d74040b00
zhougege520 <2912354876@qq.com>
2024-05-17 09:10:46 +0800

    代码提交

--- packages/designer/src/icons/layout/page/padding/Custom.jsx
+++ packages/designer/src/icons/layout/page/padding/Custom.jsx
@@ -1,16 +1,18 @@
 import { getBaseUrl } from '@utils/environmentUtil';
+
 import BaseIcon from '../../../base/BaseIcon';
 
 /**
  * 自定义
- * @param {} props 
- * @returns 
+ * @param {} props
+ * @returns
  */
 export default function (props) {
     return (
         <BaseIcon
             {...props}
-            icon={`url(${getBaseUrl()}/css/icons/layout/page/padding/custom.png)`}
+            /* icon={`url(${getBaseUrl()}/css/icons/layout/page/padding/custom.png)`} */
+            icon={`url(${getBaseUrl()}/css/icons/layout/page/padding/normal.svg)`}
         ></BaseIcon>
     );
-};
+}
--- packages/designer/src/icons/layout/page/padding/Narrow.jsx
+++ packages/designer/src/icons/layout/page/padding/Narrow.jsx
@@ -1,16 +1,18 @@
 import { getBaseUrl } from '@utils/environmentUtil';
+
 import BaseIcon from '../../../base/BaseIcon';
 
 /**
  * 窄的
- * @param {} props 
- * @returns 
+ * @param {} props
+ * @returns
  */
 export default function (props) {
     return (
         <BaseIcon
             {...props}
-            icon={`url(${getBaseUrl()}/css/icons/layout/page/padding/narrow.png)`}
+            /* icon={`url(${getBaseUrl()}/css/icons/layout/page/padding/narrow.png)`} */
+            icon={`url(${getBaseUrl()}/css/icons/layout/page/padding/normal.svg)`}
         ></BaseIcon>
     );
-};
+}
