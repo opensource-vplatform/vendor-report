ec7898bc2f393d2ca68a98746481c8b1493f9e0a
Matoah <octonber@qq.com>
2024-05-28 11:43:30 +0800

    代码提交

--- packages/excel/src/utils/varUtil.js
+++ packages/excel/src/utils/varUtil.js
@@ -1,10 +1,12 @@
 const STRING_09 = '0123456789';
 const STRING_az = 'abcdefghijklmnopqrstuvwxyz';
-const STRING_AZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const STRING_azAZ_$ = STRING_az + STRING_AZ + '_' + '$';
+//const STRING_AZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const STRING_azAZ_$ = STRING_az + '_' + '$';//STRING_az + STRING_AZ + '_' + '$';
 const STRING_09azAZ_$ = STRING_09 + STRING_azAZ_$;
 
 const TOKENS = STRING_09azAZ_$.split('');
+
+const TOKENS_SIZE = TOKENS.length;
 
 let index = 10;
 
@@ -12,10 +14,10 @@ function encode() {
     let n = index;
     let str = '';
     let k = 1;
-    while (n > 63) {
-        str = TOKENS[n % 64] + str;
-        n = parseInt(n / 64);
-        k *= 64;
+    while (n > TOKENS_SIZE-1) {
+        str = TOKENS[n % TOKENS_SIZE] + str;
+        n = parseInt(n / TOKENS_SIZE);
+        k *= TOKENS_SIZE;
     }
     if (n < 10) {
         n += 9;
