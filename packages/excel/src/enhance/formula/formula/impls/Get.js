bcfab97f52dfeebece6190ae93ea8d158366a907
zhougege520 <2912354876@qq.com>
2024-07-17 17:56:50 +0800

    代码格式化后提交

--- packages/excel/src/enhance/formula/formula/impls/Get.js
+++ packages/excel/src/enhance/formula/formula/impls/Get.js
@@ -4,18 +4,18 @@ import {
 } from '../../utils';
 
 export function execute(ast, tool) {
-    const args = ast.arguments;
-    if (!args || args.length == 0) {
-        throw Error('TOONE.GET函数未传递参数，请检查配置！');
-    }
-    let code = null,
-        fieldCode = undefined;
-    code = args[0].value;
-    fieldCode = args[1]?.value;
-    const result = tool.getValue(code, fieldCode);
-    if(result.type=='text'){
-        return toAST(result.value);
-    }else{
-        return formulaToAST(result.value);
-    }
+  const args = ast.arguments;
+  if (!args || args.length == 0) {
+    throw Error('TOONE.GET函数未传递参数，请检查配置！');
+  }
+  let code = null,
+    fieldCode = undefined;
+  code = args[0].value;
+  fieldCode = args[1]?.value;
+  const result = tool.getValue(code, fieldCode);
+  if (result.type == 'text') {
+    return toAST(result.value);
+  } else {
+    return formulaToAST(result.value);
+  }
 }
