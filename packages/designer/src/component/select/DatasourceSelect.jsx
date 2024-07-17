623a0aceb5de83ba2ff782afd58c8d906f219a9e
zhougege520 <2912354876@qq.com>
2024-07-17 15:51:27 +0800

    代码格式化提交

--- packages/designer/src/component/select/DatasourceSelect.jsx
+++ packages/designer/src/component/select/DatasourceSelect.jsx
@@ -3,23 +3,23 @@ import { useState } from 'react';
 import { Datasources } from '@components/defineDatasource/Index';
 
 export default function (props) {
-    const { onChange } = props;
-    const [searchKey, setSearchKey] = useState({
-        dsSearchKey: '',
-    });
-    return (
-        <Datasources
-            notAllowEdit={false}
-            isEditData={false}
-            searchKey={searchKey.dsSearchKey}
-            onDoubleClick={(data, parent) => {
-                if (data.type != 'table') {
-                    const formula = parent
-                        ? `TOONE.GET("${parent.code}","${data?.code}")`
-                        : `TOONE.GET("${data?.code}")`;
-                    onChange && onChange(formula);
-                }
-            }}
-        ></Datasources>
-    );
+  const { onChange } = props;
+  const [searchKey, setSearchKey] = useState({
+    dsSearchKey: '',
+  });
+  return (
+    <Datasources
+      notAllowEdit={false}
+      isEditData={false}
+      searchKey={searchKey.dsSearchKey}
+      onDoubleClick={(data, parent) => {
+        if (data.type != 'table') {
+          const formula = parent
+            ? `TOONE.GET("${parent.code}","${data?.code}")`
+            : `TOONE.GET("${data?.code}")`;
+          onChange && onChange(formula);
+        }
+      }}
+    ></Datasources>
+  );
 }
