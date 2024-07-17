b6f1b9ed921f31c9b433ca1676b37bcc6505c11b
zhougege520 <2912354876@qq.com>
2024-07-17 18:35:39 +0800

    代码格式化

--- packages/excel/src/enhance/formula/index.js
+++ packages/excel/src/enhance/formula/index.js
@@ -10,41 +10,36 @@ import { formulaToAST } from './utils';
  * @param {*} tool
  */
 export const enhance = function (formula, tool) {
-    if (isNotBlank(formula)) {
-        let ast = formulaToAST(formula);
-        const parser = parse(ast);
-        ast = parser.parse(tool);
-        formula = GC.Spread.Sheets.CalcEngine.expressionToFormula(
-            null,
-            ast,
-            0,
-            0
-        );
-        return { type: 'formula', value: formula };
-    } else {
-        return { type: 'formula', value: formula };
-    }
+  if (isNotBlank(formula)) {
+    let ast = formulaToAST(formula);
+    const parser = parse(ast);
+    ast = parser.parse(tool);
+    formula = GC.Spread.Sheets.CalcEngine.expressionToFormula(null, ast, 0, 0);
+    return { type: 'formula', value: formula };
+  } else {
+    return { type: 'formula', value: formula };
+  }
 };
 
-export const getTableCodes = function(formula){
-    const tableCodes = [];
-    if(isNotBlank(formula)){
-        let ast = formulaToAST(formula);
-        const parser = parse(ast);
-        const visitor = new Visitor({
-            function:(parser)=>{
-                const ast = parser.getAST();
-                const functionName = ast.functionName; 
-                if(functionName=="TOONE.GET"){
-                    const args = ast.arguments;
-                    if(args.length == 2){
-                        tableCodes.push(args[0].value);
-                    }
-                    return false;
-                }
-            }
-        });
-        parser.visit(visitor);
-    }
-    return tableCodes;
-}
+export const getTableCodes = function (formula) {
+  const tableCodes = [];
+  if (isNotBlank(formula)) {
+    let ast = formulaToAST(formula);
+    const parser = parse(ast);
+    const visitor = new Visitor({
+      function: (parser) => {
+        const ast = parser.getAST();
+        const functionName = ast.functionName;
+        if (functionName == 'TOONE.GET') {
+          const args = ast.arguments;
+          if (args.length == 2) {
+            tableCodes.push(args[0].value);
+          }
+          return false;
+        }
+      },
+    });
+    parser.visit(visitor);
+  }
+  return tableCodes;
+};
